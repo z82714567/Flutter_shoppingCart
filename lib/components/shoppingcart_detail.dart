cafb12848a4ce4631b4b05b457f2f096cc93a034
z82714567 <z82714567@gmail.com>
2024-02-29 10:03:33 +0900

    쇼핑카드앱 완성

--- lib/components/shoppingcart_detail.dart
+++ lib/components/shoppingcart_detail.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../constants.dart';
 
 class ShoppingCartDetail extends StatelessWidget {
   const ShoppingCartDetail({super.key});
@@ -26,24 +29,127 @@ class ShoppingCartDetail extends StatelessWidget {
   }
 
   Widget _buildDetailNameAndPrice() {
-    return SizedBox(
-      width: double.infinity,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Urban Soft AL 10.0",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "\$699",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
     );
   }
 
   Widget _buildDetailRatingAndReviewCount() {
-    return SizedBox();
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Row(
+        children: [
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Spacer(),
+          Text("review "),
+          Text("(26)", style: TextStyle(color: Colors.blue)),
+        ],
+      ),
+    );
   }
 
   Widget _buildDetailColorOptions() {
-    return SizedBox();
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Color Options"),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              _buildDetailIcon(Colors.black),
+              _buildDetailIcon(Colors.green),
+              _buildDetailIcon(Colors.orange),
+              _buildDetailIcon(Colors.grey),
+              _buildDetailIcon(Colors.white),
+            ],
+          )
+        ],
+      ),
+    );
   }
 
-  Widget _buildDetailIcon(IconData mIcon) {
-    return SizedBox();
+  Widget _buildDetailIcon(Color mColor) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10),
+      child: Stack(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(),
+              shape: BoxShape.circle,
+            ),
+          ),
+          Positioned(
+              left: 5,
+              top: 5,
+              child: ClipOval(
+                child: Container(
+                  color: mColor,
+                  width: 40,
+                  height: 40,
+                ),
+              ))
+        ],
+      ),
+    );
   }
 
   Widget _buildDetailButton(BuildContext context) {
-    return SizedBox();
+    return Align(
+        child: TextButton(
+      onPressed: () {
+        showCupertinoDialog(
+            context: context,
+            builder: (context) => CupertinoAlertDialog(
+                  title: Text("장바구니에 담으시겠습니까?"),
+                  actions: [
+                    CupertinoDialogAction(
+                      child: Text("확인"),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                ));
+      },
+      style: TextButton.styleFrom(
+          backgroundColor: kAccentColor,
+          minimumSize: Size(300, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          )),
+      child: Text(
+        "Add to Cart",
+        style: TextStyle(color: Colors.white),
+      ),
+    ));
   }
 }
